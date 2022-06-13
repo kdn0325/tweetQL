a2371e0fc0b0cd11bf0a2e54da5f602ac1452a34
kdn0325 <sexy-ehdsud@nate.com>
2022-06-13 18:36:21 +0900

    graphQL

---
+++ server.js
@@ -0,0 +1,26 @@
+import {ApolloServer , gql} from "apollo-server";
+
+const typeDefs = gql`
+    type User {
+        id:ID
+        username: String
+    }
+    type Tweet {
+        id:ID
+        text:String
+        author: User
+    }
+    type Query {
+        allTweets: [Tweet]
+        tweet(id:ID): Tweet
+    }
+    type Mutation{
+        postTweet(text:String , userId : ID): Tweet
+    }
+`;
+
+const server = new ApolloServer({typeDefs});
+
+server.listen().then(({url}) =>{
+    console.log(`Running on ${url}`);
+});
